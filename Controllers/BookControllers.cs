9d69426db44e47df19f465875d524eb074fdec15
aahmadhadi <ahmadhadijaelani@gmail.com>
2019-08-21 11:11:38 +0700

    Session 2.2

--- Controllers/BookControllers.cs
+++ Controllers/BookControllers.cs
@@ -29,6 +29,18 @@ namespace BasicWebApi.Controllers
                     Date = DateTime.UtcNow,
                     Title = "HarryPotter"
                 });
+                this.dbContext.Books.Add(new Book
+                {
+                    Id = 2,
+                    Date = DateTime.UtcNow,
+                    Title = "HarryPotter"
+                });
+                this.dbContext.Books.Add(new Book
+                {
+                    Id = 3,
+                    Date = DateTime.UtcNow,
+                    Title = "HarryPotter"
+                });
                 this.dbContext.SaveChanges();
             }
         }
@@ -36,11 +48,9 @@ namespace BasicWebApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<BookView>>> Get([FromQuery]GetBookParam param)
         {
-            //SELECT Title, Date FROM Books
             var dataSource = await dbContext.Books
                 .Select(book => new BookView 
                 {
-                    Id = book.Id,
                     Title = book.Title,
                     Date = book.Date
                 })
@@ -54,13 +64,30 @@ namespace BasicWebApi.Controllers
                 .ToList();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookView>> Get([FromRoute]int id)
+        {
+            var selectedBook = await dbContext.Books
+                .Where(book => book.Id == id)
+                .Select(book => new BookView{
+                    Date = book.Date,
+                    Title = book.Title
+                })
+                .SingleOrDefaultAsync();
+            if(selectedBook == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            return selectedBook;
+        }
+
         [HttpPost]
         public async Task<ActionResult<IEnumerable<BookView>>> Post([FromBody]BookView param)
         {
             var newId = await dbContext.Books.CountAsync() + 1;
             dbContext.Add(new Book {Id = newId, Title = param.Title, Date = param.Date });
             await this.dbContext.SaveChangesAsync();
-            return StatusCode(StatusCodes.Status201Created);
+            return CreatedAtAction(nameof(Get), new { id = newId }, param);
         }
 
         [HttpPut("{id}")]
@@ -73,6 +100,19 @@ namespace BasicWebApi.Controllers
             }
             selectedBook.Title = param.Title;
             selectedBook.Date = param.Date;
+            await dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<IEnumerable<BookView>>> Delete([FromRoute]int id)
+        {
+            var selectedBook = await dbContext.Books.SingleOrDefaultAsync(item => item.Id == id);
+            if (selectedBook == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            dbContext.Remove(selectedBook);
             await dbContext.SaveChangesAsync();
             return StatusCode(StatusCodes.Status200OK);
         }
